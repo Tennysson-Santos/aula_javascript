b252601de7dde425bf27e6c31f1fc3ebb50a9fb3
Tennysson Santos <74800388+Tennysson-Santos@users.noreply.github.com>
2020-11-27 07:22:10 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,87 @@
+
+function botao(){
+   document.getElementById("agradecimento").innerHTML = "<b>obrigado por clicar</b>";
+    
+ }
+
+ function redirecionar(){
+     window.open("https://www.uol.com.br/");
+     //window.location.href = "https://www.uol.com.br/";
+ }
+
+ function trocar(elemento){
+    /// document.getElementById("mousemove").innerHTML = "obrigado por passar o mouse!";
+     elemento.innerHTML = "obrigado por passar o mouse!";
+ }
+
+  function voltar(elemento){
+      //document.getElementById("mousemove").innerHTML = "passe o mouse aqui";
+      elemento.innerHTML = "passe o mouse aqui";
+  }
+
+   function load(){
+       alert("pagina carregada");
+   }
+
+   function funcaochange(elemento){
+     console.log(elemento.value);
+   }
+//function soma(n1, n2){
+   //return n1 + n2;
+//}
+
+//function validaidade(idade){
+  //  var validar;
+    //if (idade >= 18){
+  //  var validar = true;
+   // }else{
+    //    validar = false
+    //}
+    //return validar;
+  
+//}
+//var idade = prompt("qual é a sua idade?");
+//console.log(validaidade(idade));
+
+
+
+
+
+
+//var d = new Date();
+//alert(d.getMonth());
+//alert(d.getHours());
+//alert(d.getMinutes());
+//var count = 0;
+//while (count < 5){
+//console.log(count);
+//count = count + 1;
+//}
+
+//var idade = prompt("qual sua idade?");
+//if (idade >= 18){
+    //alert("maior de idade");
+//} else{
+    //alert("menor de idade");
+//}
+
+
+//var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}];
+//console.log(frutas);
+// alert(frutas[1].nome);
+//var lista = ["maça", "pêra", "laranja"];
+//lista.push("Uva");
+//lista.pop("laranja");
+//console.log(lista.length);
+//console.log(lista.join("|"));
+
+
+//var nome = "Tennysson Santos";
+//var n1 = 5
+//var n2= 3
+//var frase = "japão é o melhor time do mundo!!";
+//alert(nome + "tem " + idade + "anos ");
+//console.log(nome);
+//console.log(n1 * n2);
+//console.log(frase.toLowerCase());
+//alert(frase.replace("japão", "Brasil"));
